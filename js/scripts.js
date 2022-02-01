7045a978a483e707480d8f98e8072a52f2f32f1b
Luis Ribeiro <81969714+luiscpribeiro@users.noreply.github.com>
2022-02-01 19:00:23 +0000

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function newItem(){
+
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    $('#list').append(li);
+  }
+
+	li.on("dblclick", function(){
+		li.toggleClass("strike");
+	});
+
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  crossOutButton.on("click", function(){
+		li.addClass("delete")
+	});
+
+  $('#list').sortable();
+
+}
